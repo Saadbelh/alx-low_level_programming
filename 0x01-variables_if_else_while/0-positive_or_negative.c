65b807a759bffbc50e375fada14390f991e4d2d9
Saadbelh <saad.belhimer@gmail.com>
2023-08-11 16:01:34 -0700

    120 after correction

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,7 +12,7 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n>0)
+	if (n > 0)
 	{
 		printf("%d is positive\n", n);
 	}
@@ -25,4 +25,4 @@ int main(void)
 		printf("%d is negative\n", n);
 	}
 	return (0);
-}	
+}
--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -9,7 +9,7 @@ int main(void)
 	char alp[26] = "abcdefghijklmnopqrstuvwyz";
 	int i;
 
-	for (i=0; i<26; i++)
+	for (i = 0; i < 26; i++)
 	{
 		putchar(alp[i]);
 	}
