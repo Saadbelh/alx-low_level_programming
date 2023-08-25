96cdb4b4bc1bc831bac8bd929e1608144cbc7485
Saadbelh <saad.belhimer@gmail.com>
2023-08-25 09:49:49 -0700

    re

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -6,13 +6,14 @@
  */
 char *string_toupper(char *n)
 {
-	int i;
+	int i = 0;
 
 	while (n[i] != '\0')
 	{
 		if (n[i] >= 'a' && n[i] <= 'z')
-			n[i] = n[i] - 32;
+			n[i] -=  32;
 		i++;
 	}
+
 	return (n);
 }
