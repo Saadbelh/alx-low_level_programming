0fcc7f00a8b2f6a19f391594efac3f1064f6705a
Saadbelh <saad.belhimer@gmail.com>
2023-09-12 18:54:09 -0700

    ki

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOC_H
+#define DOG_H
+
+/**
+ * struct dog - dog
+ * @name: name
+ * @age: age
+ * @owner: owner
+ *
+ * Description: description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner:
+};
+
+#endif
